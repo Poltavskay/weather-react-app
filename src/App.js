5348c04a1ab09921c57146aeb246ceb42f9679d9
Poltavskay <poltavskay9@gmail.com>
2023-07-23 17:41:07 +0300

    Remove unessary files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Weather from "./Weather"
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="container">
+        <Weather defaultCity="Kyiv" />
+        <footer>
+          This project was coded by Galina Poltavskay and is{" "}
+          <a
+            href="https://github.com/Poltavskay/react-site"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
